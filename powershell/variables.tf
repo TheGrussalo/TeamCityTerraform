37181e474fb86a001a907487d80f40f3135a2438
Gruss <TheGrussalo@TheGrussalo.com>
2019-04-24 16:13:22 +0100

    Updated for TC variables

--- powershell/variables.tf
+++ powershell/variables.tf
@@ -1,4 +1,4 @@
-variable "arm_subscription_id" {$env:ARM_SUBSCRIPTION_ID}
-variable "arm_principle" {$env:ARM_CLIENT_ID} 
-variable "arm_password" {$env.ARM_CLIENT_SECRET}
-variable "tenant_id" {$env:ARM_TENANT_ID}
+variable "arm_subscription_id" {${ARM_SUBSCRIPTION_ID}}
+variable "arm_principle" {${ARM_CLIENT_ID} }
+variable "arm_password" {${ARM_CLIENT_SECRET}}
+variable "tenant_id" {${ARM_TENANT_ID}}
