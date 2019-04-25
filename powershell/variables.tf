5652ef7d74e69e8ec55be3e257a23b42d3b28247
Gruss <TheGrussalo@TheGrussalo.com>
2019-04-25 09:44:19 +0100

    Trying percent symbol

--- powershell/variables.tf
+++ powershell/variables.tf
@@ -1,4 +1,4 @@
-variable "arm_subscription_id" {"${ARM_SUBSCRIPTION_ID}"}
-variable "arm_principle" {"${ARM_CLIENT_ID}"}
-variable "arm_password" {"${ARM_CLIENT_SECRET}"}
-variable "tenant_id" {"${ARM_TENANT_ID}"}
+variable "arm_subscription_id" {"%ARM_SUBSCRIPTION_ID%"}
+variable "arm_principle" {"%ARM_CLIENT_ID%"}
+variable "arm_password" {"%ARM_CLIENT_SECRET%"}
+variable "tenant_id" {"%ARM_TENANT_ID%"}
