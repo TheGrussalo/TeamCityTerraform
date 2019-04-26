cf52bad6472e8eeaa279979beacabb6788fd9181
Gruss <TheGrussalo@TheGrussalo.com>
2019-04-26 13:25:54 +0100

    Added auto-approve to the correct file

--- tf/plan.ps1
+++ tf/plan.ps1
@@ -1,1 +1,1 @@
-tools\terraform\terraform.exe plan -auto-approve -var arm_subscription_id=${ARM_SUBSCRIPTION_ID} -var arm_principle=${ARM_CLIENT_ID} -var arm_password=${ARM_CLIENT_SECRET} -var tenant_id=${ARM_TENANT_ID} tf
+tools\terraform\terraform.exe plan -var arm_subscription_id=${ARM_SUBSCRIPTION_ID} -var arm_principle=${ARM_CLIENT_ID} -var arm_password=${ARM_CLIENT_SECRET} -var tenant_id=${ARM_TENANT_ID} tf
